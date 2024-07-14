72e3d40feb7b58edcc96e0272c08c8a3cb2c707b
alexosterhoudt <alex_osterhoudt94@yahoo.com>
2024-07-14 14:40:55 -0400

    Added initial files.

---
+++ Scripts/player.cs
@@ -0,0 +1,65 @@
+using Godot;
+using System;
+
+public partial class player : CharacterBody2D
+{
+
+	const float SPEED = 300f;
+	public AnimatedSprite2D animatedSprite;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+
+		if(Input.IsKeyPressed(Key.Escape))
+		{
+			GetTree().Quit();
+
+		}
+
+		var direction = Input.GetAxis("move_left", "move_right");
+		var pos = Velocity.X + direction;
+
+		if(direction > 0)
+		{
+			animatedSprite.FlipH = false;
+		}
+		else if(direction < 0)
+		{
+			animatedSprite.FlipH = true;
+		}
+
+		if(direction == 0)
+		{
+			animatedSprite.Play("Idle");
+		}
+		else
+		{
+			animatedSprite.Play("Run");
+		}
+
+		if(Input.IsActionPressed("attack", false))
+		{
+			animatedSprite.Play("Attack");
+		}
+
+		if(direction != 0)
+		{
+			Velocity = new Vector2(direction, 0) * SPEED;
+		}
+		else
+		{
+			Velocity = Vector2.Zero;
+		}
+		MoveAndSlide();
+
+	}
+
+}
